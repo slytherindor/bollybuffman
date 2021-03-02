de7856cde99b4126b179b4aeda9f650e82fe1526
Shubhanshu <shubh.aarsay@gmail.com>
2021-03-02 16:09:34 -0500

    Moved stuff out of main py to let the game run without deps.

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import asyncio
-
-from config import load_dot_env_file, load_env_vars_to_config
-from fetch_movie import fetch_movie
 from game import Game
 from movie import Movie
 
@@ -17,15 +13,6 @@ def game_run():
     print(game.result())
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-    load_dot_env_file()
-    config = load_env_vars_to_config()
-    asyncio.run(fetch_movie(config, 'hi'))
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     game_run()
-
