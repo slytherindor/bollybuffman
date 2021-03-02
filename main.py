9d652f4cc88e7adf04f7775a2fe13be1d655ae91
Shubhanshu <shubh.aarsay@gmail.com>
2021-03-02 15:59:48 -0500

    Adding main file for some reference.

---
+++ main.py
@@ -0,0 +1,36 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import asyncio
+
+from config import load_dot_env_file, load_env_vars_to_config
+from fetch_movie import fetch_movie
+from game import Game
+from movie import Movie
+
+
+def game_run():
+    movie = Movie("Hera Pheri")
+    game = Game(movie)
+    while game.is_playable():
+        letter = input("Enter letter:\n")
+        game.play(letter)
+        print(game)
+
+    print(game.result())
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    load_dot_env_file()
+    config = load_env_vars_to_config()
+    asyncio.run(fetch_movie(config, 'hi'))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
